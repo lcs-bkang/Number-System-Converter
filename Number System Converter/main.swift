4ccb55b3c03cbb98d9e284a6bb957e9bb46e61d0
Bradley Kang <bkang@lcs.on.ca>
2021-01-26 15:35:17 -0500

    Jan 26/2021 #2

--- Number System Converter/main.swift
+++ Number System Converter/main.swift
@@ -63,33 +63,33 @@ default:
 func numberConverter(numberToConvert value: Int, fromBase base: Int, toBase: Int) -> Double {
     // Exponent value at the rightmost digit
     var exponent = 0.0
-    
+    var valueString = String(value)
     // Current sum in decimal
     var decimalEquivalent = 0.0
     // Iterate over each character
     // From right to left
     // For...in will loop the exact right amount of times.
     
-    for character in value.reversed() {
-        if base == 16.0 {
+    for character in valueString.reversed() {
+        if base == 16 {
             // If conversions to doubles work, it is 0-9
             if let digit = Double(String(character)) {
-                decimalEquivalent += digit * pow(se, exponent)
+                decimalEquivalent += digit * pow(Double(base), exponent)
             } else {
                 // It can't be switched to a double, so its A-F
                 switch character {
                 case "A":
-                    decimalEquivalent += 10 * pow(se, exponent)
+                    decimalEquivalent += 10 * pow(Double(base), exponent)
                 case "B":
-                    decimalEquivalent += 11 * pow(se, exponent)
+                    decimalEquivalent += 11 * pow(Double(base), exponent)
                 case "C":
-                    decimalEquivalent += 12 * pow(se, exponent)
+                    decimalEquivalent += 12 * pow(Double(base), exponent)
                 case "D":
-                    decimalEquivalent += 13 * pow(se, exponent)
+                    decimalEquivalent += 13 * pow(Double(base), exponent)
                 case "E":
-                    decimalEquivalent += 14 * pow(se, exponent)
+                    decimalEquivalent += 14 * pow(Double(base), exponent)
                 case "F":
-                    decimalEquivalent += 15 * pow(se, exponent)
+                    decimalEquivalent += 15 * pow(Double(base), exponent)
                 default:
                     break
                 }
